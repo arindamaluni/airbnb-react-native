845c3f17320c445f9400df9b8efbaa9baf222076
arindamaluni <aaluni@gmail.com>
2021-01-11 17:01:34 +0000

    Map sync with carousel

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useWindowDimensions, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
@@ -9,9 +9,37 @@ import PostCarouselItem from '../../components/PostCarrouselItem';
 const SearchResultsMap = () => {
   const width = useWindowDimensions().width;
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+  const flatList = useRef();
+  const mapRef = useRef();
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      console.log(viewableItems[0].item.id);
+      setSelectedPlaceId(() => viewableItems[0].item.id);
+    }
+  });
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatList) {
+      return;
+    }
+    const index = places.findIndex((place) => place.id === selectedPlaceId);
+    flatList.current.scrollToIndex({index});
+
+    const currentPlace = places[index];
+    const region = {
+      latitude: currentPlace.coordinate.latitude,
+      longitude: currentPlace.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    };
+    mapRef.current.animateToRegion(region);
+  }, [selectedPlaceId]);
+
   return (
     <View style={{height: '100%', width: '100%'}}>
       <MapView
+        ref={mapRef}
         style={{height: '100%', width: '100%'}}
         provider={PROVIDER_GOOGLE} //Req for running google in iOS
         initialRegion={{
@@ -22,7 +50,6 @@ const SearchResultsMap = () => {
         }}>
         {places.map((place) => (
           <CustomMarker
-            clickHandler={setSelectedPlaceId}
             key={place.id}
             coordinate={place.coordinate}
             price={place.newPrice}
@@ -34,6 +61,7 @@ const SearchResultsMap = () => {
       <View style={{position: 'absolute', bottom: 10}}>
         {/* <PostCarouselItem post={places[0]} /> */}
         <FlatList
+          ref={flatList}
           data={places}
           renderItem={({item}) => <PostCarouselItem post={item} />}
           horizontal
@@ -41,6 +69,8 @@ const SearchResultsMap = () => {
           snapToInterval={width - 60}
           snapToAlignment={'center'}
           decelerationRate={'fast'}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
